0a411f4825719ebbd105916e5a0da356ae8ba0a9
Itay Almog <itay2828@gmail.com>
2021-03-12 20:17:36 +0200

    comments

--- virtdbg/virtdbg.h
+++ virtdbg/virtdbg.h
@@ -9,10 +9,19 @@ typedef struct virtdbg_args {
     // space at the end of the binary itself that can be
     // used for dynamic memory allocation
     uintptr_t stolen_memory_base;
+
+    // this should point to the area after the virtdbg binary
+    // AND after the page tables and stack that the loader
+    // allocated to the hypervisor
     uintptr_t virtdbg_end;
+
+    // this is the end of the stolen memory, anything between the prev
+    // variable to this can be used by the hypervisor for whatever
+    // dynamic memory it may need
     uintptr_t stolen_memory_end;
 
-    // the return address of the loader
+    // the return address of the loader, once vmx is initialized this is
+    // where the hypervisor will load everything to
     uintptr_t return_address;
 } virtdbg_args_t;
 
